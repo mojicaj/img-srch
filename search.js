e7d4e405f4a2dbf576f5cd0651951aaf36f362c0
Glitch (pebble-harmonica) <none>
2017-11-22 21:49:29 +0000

    Checkpoint

--- search.js
+++ search.js
@@ -10,8 +10,16 @@ let url = 'https://cse.google.com/cse.js?key='+ key +'&cx=' + id +'&q=';
 module.exports = (query) => {
   return new Promise( (resolve, reject) => {
     
-    http.get(url+query, (data) => {
-      
+    http.get(url+query, (response) => {
+      response.on("error", function (err) {
+        return reject(err);
+      });
+    
+    response.setEncoding('utf8');
+    var data = [];
+    response.on("data", function (chunk) { data.push(chunk) });
+    
+    response.on("end", function () { data.forEach(function (res) { console.log(res) }) });
     })
     resolve({ query })
   
