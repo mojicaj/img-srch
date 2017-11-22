84e6e30e602f86f048e0c37348c9e490cc59cbd5
Glitch (pebble-harmonica) <none>
2017-11-22 22:22:29 +0000

    Checkpoint

--- search.js
+++ search.js
@@ -10,18 +10,21 @@ let url = 'https://www.googleapis.com/customsearch/v1?key='+ key +'&cx=' + id +'
 module.exports = (query) => {
   return new Promise( (resolve, reject) => {
     
-    http.get(url+query, (response) => {
+    http.get(url+query, function (response) {
+      console.log(response);
       response.on("error", function (err) {
-        console.log('error:'+err)
+        console.log('error:'+err);
         return reject(err);
       });
     
     //response.setEncoding('utf8');
     let data;
-    response.on("data", function (chunk) { data = chunk });
-      console.log(data);
+    response.on("data", function (chunk) { console.log('chunk:'+chunk)
+                                          data = chunk });
+      //console.log(data);
     
-    response.on("end", function () { resolve({ data }) });
+    response.on("end", function () { console.log('end:');
+                                                 resolve({ data }) });
     })
   
     /*mongodb.MongoClient.connect(uri, function(err, db) {
