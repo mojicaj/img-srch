d179184e72232c273c31d564aa825af6b08acbd6
Glitch (pebble-harmonica) <none>
2017-11-23 20:41:32 +0000

    Checkpoint

--- search.js
+++ search.js
@@ -6,46 +6,25 @@ const key = process.env.AKEY;
 const id = process.env.AID;
 const https = require('https');
 
-/*
-let test = function (query) {
-  //return { "yup": query }
-  
-  let url = 'https://www.googleapis.com/customsearch/v1?key='+ key +'&cx=' + id +'&q='+query;
-  
-    https.get(url, function  (response) {
-      console.log('in');
-    
-    //response.setEncoding('utf8');
-    let data = '';
-    response.on("data", function (chunk) { console.log('chunkJ:'+chunk.items)
-                                          data += chunk });
-      //console.log(data);
-    
-    response.on("end", function () { resolve(data)});
-    }).on('error', (err) => {console.log('rejected:' +err)});
-}*/
-
-
-//module.exports = test;
 
 module.exports = (query) => {
   return new Promise( (resolve, reject) => {
     let url = 'https://www.googleapis.com/customsearch/v1?key='+ key +'&cx=' + id +'&q='+query;
     
     https.get(url, function  (response) {
-    //  response.setEncoding('utf8');
+    response.setEncoding('utf8');
     
-      let data = '';
+      let rawData = '';
     
-      response.on("data", function (chunk) { data += chunk });  //console.log('chunk:'+chunk)    
+      response.on("data", function (chunk) { rawData += chunk });  //console.log('chunk:'+chunk)    
     
       response.on("end", function () { 
-        console.log('end:');
-        console.log("data: "+data);
-        resolve(data);
+        let data = JSON.parse(rawData);
+        console.log(data.items);
+        resolve(data.items);
         
       });
-    }).on('error', (err) => {console.log('rejected:' +err)});
+    }).on('error', (err) => {reject(err)});
   
     /*mongodb.MongoClient.connect(uri, function(err, db) {
       if(err) return reject(err);
