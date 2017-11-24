0ba34e91bb921ebf0581093a48b93a6b9f874680
Glitch (pebble-harmonica) <none>
2017-11-24 18:02:59 +0000

    Checkpoint

--- latest.js
+++ latest.js
@@ -3,4 +3,22 @@
 const mongodb = require('mongodb');
 const uri = "mongodb://"+process.env.USER+":"+process.env.PASS+"@"+process.env.HOST+":"+process.env.DB_PORT+"/"+process.env.DB;
 
-module.ex
+module.exports = () => {
+  
+  return new Promise( (resolve, reject) => {
+    
+    mongodb.MongoClient.connect(uri, function(err, db) {
+      if(err) return reject(err);
+      
+      const searches = db.collection('searches');
+      
+      searches.find({_id: 0}).toArray(function (err, stored) {
+        if (err) reject(err);
+        console.log(stored[0]);
+        resolve('searches');
+      });
+      
+      db.close();
+    });
+  });
+}
